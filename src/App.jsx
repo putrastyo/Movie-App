90f9d8d9513383f145cb556f30094fd6d7a1f383
putrastyo <me.putrasetyo@gmail.com>
2023-10-29 13:32:32 +0700

    http to https

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MovieCard from './components/MovieCard'
 import Header from './components/Header'
 import './App.css'
 
-const API_URL = "http://www.omdbapi.com/?apikey=bd58e087"
+const API_URL = "https://www.omdbapi.com/?apikey=bd58e087"
 
 const App = () => {
   const [movies, setMovies] = useState([])
